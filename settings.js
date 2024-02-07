46771190ab56d40467063c0cbaf751d09983aff2
Senaugra <155700857+Senaugra@users.noreply.github.com>
2024-02-07 08:59:54 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Aqsoapi'// yourname
+global.MONGO_DB_URI = "mongodb+srv://aqsobonde:<password>@aqso.6sdflwt.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "evanfarandi474@gmail.com" //email
+global.email_password = "BABOTAKJO" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
